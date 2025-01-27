75567bf5f3806404184df7f7ef8987a4c2d58ea2
Jérémy <jeremy.perrouault@gmail.com>
2025-01-27 11:08:28 +0000

    JOUR 1 - EXO BITWISE

---
+++ exo-bitwise-boolean/BitwiseApplication.java
@@ -0,0 +1,26 @@
+public class BitwiseApplication {
+    public static void main(String[] args) {
+        boolean bool1 = true;
+        boolean bool2 = false;
+        boolean bool3 = true;
+        byte result = 0;
+
+        if (bool1) {
+            result = 1 << 2;
+        }
+
+        if (bool2) {
+            result |= 1 << 1;
+        }
+
+        if (bool3) {
+            // result |= 1 << 0;
+            result |= 1;
+        }
+
+        result = (byte)(((bool1 ? 1 : 0) << 2) | ((bool2 ? 1 : 0) << 1) | (bool3 ? 1 : 0));
+
+        System.out.println("Valeur : " + result);
+        System.out.println("Valeur binaire : " + Integer.toBinaryString(result));
+    }
+}

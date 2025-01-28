317303f51349b02d01ed19cf8dfe61bc0799b4ff
Jérémy <jeremy.perrouault@gmail.com>
2025-01-28 11:09:47 +0000

    JOUR 2 - BUILDER

--- exo-builder/Application.java
+++ exo-builder/Application.java
@@ -1,7 +1,14 @@
 public class Application {
     public static void main(String[] args) {
-        DataSource ds = new DataSource(true, "hostname", 3306, "username", "123456", "i1", "i2", "i3", "i4", "i5", "i6");
+        // DataSource ds = new DataSource(true, "hostname", 3306, "username", "123456", "i1", "i2", "i3", "i4", "i5", "i6");
 
-
+        DataSource ds = DataSource.builderV2()
+            .host("host")
+            .port(333)
+            .password("secret")
+            .username("username")
+            .secure(true)
+            .build()
+        ;
     }
 }
--- exo-builder/DataSource.java
+++ exo-builder/DataSource.java
@@ -4,28 +4,101 @@ public class DataSource {
     private String username;
     private String password;
     private boolean secure;
-    private String info1;
-    private String info2;
-    private String info3;
-    private String info4;
-    private String info5;
-    private String info6;
 
-    public DataSource(boolean secure, String username, int port, String host, String password, String info1, String info2, String info3, String info4, String info6, String info5) {
+    public DataSource() { }
+
+    public DataSource(boolean secure, String username, int port, String host, String password) {
         this.host = host;
         this.port = port;
         this.username = username;
         this.password = password;
 
-        if (host.isEmpty()) {
-            throw new RuntimeException("euh vide interdit nan mais oh");
+        // if (host.isEmpty()) {
+        //     throw new RuntimeException("euh vide interdit nan mais oh");
+        // }
+    }
+
+    public static DataSourceBuilderV1 builderV1() {
+        return new DataSourceBuilderV1();
+    }
+
+    public static DataSourceBuilderV2 builderV2() {
+        return new DataSourceBuilderV2();
+    }
+
+    public static class DataSourceBuilderV1 {
+        private DataSource instance = new DataSource();
+
+        public DataSource build() {
+            return this.instance;
         }
 
-        this.info1 = info1;
-        this.info2 = info2;
-        this.info3 = info3;
-        this.info4 = info4;
-        this.info5 = info5;
-        this.info6 = info6;
+        public DataSourceBuilderV1 host(String host) {
+            if (host.isEmpty()) {
+                throw new RuntimeException("euh vide interdit nan mais oh");
+            }
+
+            this.instance.host = host;
+            return this;
+        }
+
+        public DataSourceBuilderV1 port(int port) {
+            this.instance.port = port;
+            return this;
+        }
+        public DataSourceBuilderV1 username(String username) {
+            this.instance.username = username;
+            return this;
+        }
+
+        public DataSourceBuilderV1 password(String password) {
+            this.instance.password = password;
+            return this;
+        }
+
+        public DataSourceBuilderV1 secure(boolean secure) {
+            this.instance.secure = secure;
+            return this;
+        }
+    }
+
+    public static class DataSourceBuilderV2 {
+        private String host;
+        private int port;
+        private String username;
+        private String password;
+        private boolean secure;
+
+        public DataSource build() {
+            return new DataSource(secure, username, port, host, password);
+        }
+
+        public DataSourceBuilderV2 host(String host) {
+            if (host.isEmpty()) {
+                throw new RuntimeException("euh vide interdit nan mais oh");
+            }
+
+            this.host = host;
+            return this;
+        }
+
+        public DataSourceBuilderV2 port(int port) {
+            this.port = port;
+            return this;
+        }
+        public DataSourceBuilderV2 username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public DataSourceBuilderV2 password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public DataSourceBuilderV2 secure(boolean secure) {
+            this.secure = secure;
+            return this;
+        }
     }
 }
